0d7a6b3d52e406d0920c570167b2cd6a5d709ebf
bo <bo@qq.com>
2021-08-07 22:52:36 +0800

    first

---
+++ src/main/java/com/bo/Hello.java
@@ -0,0 +1,12 @@
+package com.bo;
+
+/**
+ * @author Bo
+ * @version 1.0
+ * @date 2021/8/7 22:47
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
